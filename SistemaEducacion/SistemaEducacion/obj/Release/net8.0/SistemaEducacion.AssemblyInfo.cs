09eac999803a043dfa7259a6576abafd3366db65
anthonyruizcr <anthonyrg3124@hotmail.com>
2024-03-29 21:00:32 -0600

    Prueba

--- SistemaEducacion/SistemaEducacion/obj/Release/net8.0/SistemaEducacion.AssemblyInfo.cs
+++ SistemaEducacion/SistemaEducacion/obj/Release/net8.0/SistemaEducacion.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("aspnet-SistemaEducacion-dc892aac-de8d-4b7d-b446-322fcaca1089")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaEducacion")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaEducacion")]
--- SistemaEducacion_API/SistemaEducacion_API/obj/Release/net8.0/SistemaEducacion_API.AssemblyInfo.cs
+++ SistemaEducacion_API/SistemaEducacion_API/obj/Release/net8.0/SistemaEducacion_API.AssemblyInfo.cs
@@ -11,14 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
-[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("aspnet-SistemaEducacion-dc892aac-de8d-4b7d-b446-322fcaca1089")]
-[assembly: System.Reflection.AssemblyCompanyAttribute("SistemaEducacion")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("SistemaEducacion_API")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("SistemaEducacion")]
-[assembly: System.Reflection.AssemblyTitleAttribute("SistemaEducacion")]
+[assembly: System.Reflection.AssemblyProductAttribute("SistemaEducacion_API")]
+[assembly: System.Reflection.AssemblyTitleAttribute("SistemaEducacion_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
